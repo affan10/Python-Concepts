11d4d09eb9b7937214d6b99196eccc7f05604db0
Affan Muddassir <affan.muddassir10@gmail.com>
2020-03-29 18:07:05 +0500

    Add files via upload

---
+++ comprehensions.py
@@ -0,0 +1,45 @@
+'''
+    For List Comprehension
+'''
+nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+# Get square of all numbers
+result = [n * n for n in nums]
+print(result)
+
+# Get only those numbers that are even
+result = [n for n in nums if n % 2 == 0]
+print(result)
+
+# Get (letter, number) for each number in '0123' and 'abcd'
+result = [(letter, number) for letter in 'abcd' for number in range(4)]
+print(result)
+
+'''
+    For Dictionary Comprehension
+'''
+# Create a dictionary with corresponding names and surnames
+names     = ['Lionel', 'Andres', 'Xavi', 'Carles', 'Gerard']
+surnames  = ['Messi', 'Iniesta', 'Hernandez', 'Puyol', 'Pique']
+zipResult = list(zip(names, surnames))
+zipResult = zipResult[:]
+print(zipResult)
+
+fullNames = {name:surname for name, surname in zip(names, surnames)}
+print(fullNames)
+
+# Create a dictionary without Pique
+fullNames = {name:surname for name, surname in zip(names, surnames) if surname != 'Pique'}
+print(fullNames)
+
+'''
+    For Set Comprehensions
+'''
+# Add numbers to Set
+nums   = [1, 1, 2, 3, 3, 4, 5, 6, 7, 8, 9, 9, 10, 11, 12, 13, 13]
+datSet = {n for n in nums}
+print(datSet)
+
+# Add only even numbers to Set
+datSet = {n for n in nums if n % 2 == 0}
+print(datSet)
